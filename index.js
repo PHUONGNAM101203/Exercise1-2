86594bb15c9854dfe660c5414e020f8d67b8c92b
PHUONGNAM101203 <pnamhuynhle@gmail.com>
2024-05-07 14:30:24 +0700

    solve

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-var rect = {
-    perimeter:(x,y) => (2*(x+y)),
-    area:(x,y) => (x*y)
-
-};
+var rect = require('./rectangle');
 function solveRect(l,b) {
     console.log("Solving for rectangle with l = " + l + ", b = " + b);
-
-    if (l <=0 || b <=0) {
-        console.log("Rectangle dimensions be greater than zero: l=" + l + ", b=" + b);
-    }
-    else
-    {
-        console.log("Area = " + rect.area(l,b));
-        console.log("Perimeter = " + rect.perimeter(l,b));
-    }
-}
+    rect(l,b, (err,rectangle) =>  {
+        if (err) {
+            console.log(err.message);
+        }
+        else {
+           console.log("The area of rectangle of dimensions l=" + l + ", b=" + b + " is " + rectangle.area());
+           console.log("The perimeter of rectangle of dimensions l=" + l + ", b=" + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement after the call to rect()");
+};
 solveRect(2,4);
 solveRect(3,5);
 solveRect(0,5);
